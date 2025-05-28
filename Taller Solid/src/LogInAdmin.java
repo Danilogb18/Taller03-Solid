80fe865b543cf756f5800cab3ec9d06263e25f88
Danilo <>
2025-05-28 14:37:49 -0500

    LogInAdmin solucionado, Liskov

--- Taller Solid/src/LogInAdmin.java
+++ Taller Solid/src/LogInAdmin.java
@@ -1,16 +1,16 @@
 public class LogInAdmin extends LogIn {
     private boolean userIsAdmin;
+
     @Override
-    public void log (User user) {
+    public void log(User user) {
         this.userIsAdmin = verifyIfTheUserIsAdmin(user);
-        if(!userIsAdmin){
-            return;
+        if (userIsAdmin) {
+            System.out.println("Has access to the website in admin mode");
         }
-        System.out.println("Has access to the website in admin mode");
-        // Logic
+        super.log(user);  // Esto asegura que la lógica de la clase base también se ejecute
     }
-    private boolean verifyIfTheUserIsAdmin(User user){
-        // Do something
+
+    private boolean verifyIfTheUserIsAdmin(User user) {
         return true;
     }
 }
